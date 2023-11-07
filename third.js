57ddcd367ac7a4616235da9ded7ff57b089fc162
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-11-07 14:42:28 +0545

    to enter marks of students

--- third.js
+++ third.js
@@ -1,5 +1,15 @@
-const ps = require("prompt-sync")
-const prompt = ps();
-let name=prompt("Enter your name..");
-let age = parseInt(prompt("Enter your age.."));
-console.log("your name is " +name+" and your age is " +age);
+const prompt = require("prompt-sync")();
+
+let name = prompt("Enter your name: ");
+let age = parseInt(prompt("Enter your age: "));
+console.log("Your name is " + name + " and your age is " + age);
+
+let n = parseInt(prompt("Enter the number of subjects: "));
+let array = [];
+for (let i = 0; i < n; i++) {
+    array[i] = parseInt(prompt("Enter marks of subject " + (i + 1) + ": "));
+}
+
+for (let i = 0; i < n; i++) {
+    console.log("Marks of subject " + (i + 1) + ": " + array[i]);
+}
