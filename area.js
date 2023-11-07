4501a8ae354ad28b79a238804aca14c1ae7344a5
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-11-07 16:26:45 +0545

    area of circle repeat

--- area.js
+++ area.js
@@ -1,5 +1,11 @@
 const prompt = require("prompt-sync")();
 
+while (true) {
+  let r = parseInt(prompt("Enter radius of circle: "));
+  console.log("The are of given circle is  ", 3.1415 * r * r);
 
-let r = parseInt(prompt("Enter radius of circle: "));
-console.log("The are of given circle is  ", 3.1415*r*r);
+  let continuePrompt = prompt("Do you want to continue? (yes/no): ");
+  if (continuePrompt.toLowerCase() !== "yes") {
+    break;
+  }
+}
