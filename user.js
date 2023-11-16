7801f866ed8fa695ab71b04364c29e7eb070b6f9
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-11-16 15:53:07 +0545

    user input

---
+++ user.js
@@ -0,0 +1,32 @@
+const prompt = require("prompt-sync")();
+let name = prompt("Enter your full name: ");
+console.log("your name is " + name);
+
+let Num = parseInt(prompt("Enter any Numbers: "));
+console.log("your number is " + Num);
+
+let Letter = prompt("Enter any Alphabet: ")[0];
+console.log("your alphabet is " + Letter);
+
+// enter void input
+
+let inputs = prompt("Enter your input ");
+console.log("your inputs are " + inputs);
+// allocate the address
+for (let i = 0; i < inputs.length; i++)
+  console.log("your data in " + i + " is " + inputs[i]);
+
+
+// array input
+let no = parseInt(prompt("Enter no of elements: "));
+let array = [];
+console.log("Enter your array elements..");
+for (let n = 0; n < no; n++) {
+  array[n] = prompt();
+}
+
+console.log("your array elements are " + array);
+console.log("your 2nd element in array is " + array[1]);
+for (let n = 0; n < no; n++) {
+  console.log("your element in " + n + " is " + array[n]);
+}
