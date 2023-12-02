660d8168f44dee63068cba5b537099d7d8cf5154
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-12-02 22:18:07 +0545

    initialization

--- compound.js
+++ compound.js
@@ -3,7 +3,7 @@ let p = parseInt(prompt("Enter principle amount: "));
 let t = parseInt(prompt("Enter time period for : "));
 let r = parseInt(prompt("Enter rate of interest: "));
 
-
+// push
 let a = p * (1 + r / 100) ^ (t);
 let c = a-p;
 
