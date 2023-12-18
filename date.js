8a4abc7612c2010292cd109e4601d4a8dd5f5a26
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-12-18 23:40:10 +0545

    good date conversion

--- date.js
+++ date.js
@@ -20,17 +20,17 @@ function convertToAD() {
   if (bsDay > 17) {
     bsDay -= 17;
   } else {
-    bsDay += 13;
-    bsMonth--;
+    bsDay += 30;
+    --bsMonth;
     bsDay -= 17;
   }
 
-  if (bsMonth > 9) { // corrected line
-    bsMonth -= 9; // corrected line
+  if (bsMonth > 8) {
+    bsMonth -= 8;
   } else {
-    bsMonth += 3;
-    bsYear--;
-    bsMonth += 9;
+    bsMonth += 12;
+    --bsYear;
+    bsMonth -= 8;
   }
 
   bsYear -= 56;
@@ -38,37 +38,29 @@ function convertToAD() {
   // Display the converted date in the resultAD div
   resultAD.textContent = `Converted AD Date: ${bsYear}-${bsMonth}-${bsDay}`;
   // Clear the resultBS div
-  resultBS.textContent = '';
+  resultBS.textContent = "";
 }
 
 function convertToBS() {
   let adDate = inputDate.value;
   let [adYear, adMonth, adDay] = adDate.split("-").map(Number);
 
-  adYear += 56;
-
-  if (adMonth > 8) {
-    adMonth -= 8;
+  if (adDay < 13) {
+    adDay += 17;
   } else {
-    adMonth += 4;
-    adYear--;
+    adDay -= 13;
+    ++adMonth;
   }
-
-  if (adDay > 16) {
-    adDay -= 16;
-  } else {
-    adDay += 30;
-    adMonth--;
-    adDay -= 16;
-  }
-
-  if (adMonth < 5) {
+  if (adMonth < 4) {
     adMonth += 8;
-    adYear--;
+  } else {
+    adMonth -= 4;
+    ++adYear;
   }
+  adYear += 56;
 
   // Display the converted date in the resultBS div
   resultBS.textContent = `Converted BS Date: ${adYear}-${adMonth}-${adDay}`;
   // Clear the resultAD div
-  resultAD.textContent = '';
+  resultAD.textContent = "";
 }
